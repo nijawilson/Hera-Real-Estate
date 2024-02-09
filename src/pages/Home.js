acf4b9b262f88f0e5794305fb45ecbd2d8b70dae
nijawilson <nijdogz@gmail.com>
2024-02-08 20:45:40 -0500

    created React App

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import './styles/Home.css';
+
+function Home() {
+    return(
+        <div className="home-div">
+            <section className="main">
+                <h1>Where Home Find Their Stories
+                </h1>
+                <p className="homeP">
+                    Lorem
+                </p>
+            </section>
+           
+        </div>
+    )
+}
+
+export default Home;
